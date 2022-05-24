a200f758a7729781f7cadbb6c6518f6e2d39eefe
simonalbcc <100119048+simonalbcc@users.noreply.github.com>
2022-05-24 18:18:11 +0200

    Correction phase 1

--- Projet/FileManagement.c
+++ Projet/FileManagement.c
@@ -2,7 +2,7 @@
 
 
 void gendersArray(int subjectsGender[]) {
-	FILE* fiGender;                                                        // donner un nom plus significatif -> corrigé 
+	FILE* fiGender;                                                        
 	Someone user;
 	bool correct = true;
 
@@ -13,22 +13,20 @@ void gendersArray(int subjectsGender[]) {
 	if (fiGender == NULL) {
 		printf("Erreur a l'ouverture du fichier !");
 	}else {
-		while (!feof(fiGender)) {							// je ne comprends pas à quoi sert cette boucle ? De plus, ça ne correspond pas à votre DA
-			int iSubject = 0;								// la boucle sert à aller chercher les infos demandées dans le doc data_subjects_info, DA à corriger 
-			//lire ligne d'entête
-			fgets(line, SIZE_LINE, fiGender);
-			//lecture du genre de chaque ligne
-			fgets(line, SIZE_LINE, fiGender); 
-			while (!feof(fiGender) && correct) {
-				if (sscanf_s(line, "%d,%d,%d,%d,%d", &user.code, &user.age, &user.weight, &user.height, &user.gender) == 5) {
-					subjectsGender[iSubject] = user.gender;
-					iSubject++;
-				}else {
-					correct = false;
-					printf("Erreur a la lecture des genres des sujets");   // et peut-être sortir directement de la boucle si erreur -> corrigé 
-				}
-				fgets(line, SIZE_LINE, fiGender);
+		//lire ligne d'entête
+		fgets(line, SIZE_LINE, fiGender);
+		int iSubject = 0;
+		//lecture du genre de chaque ligne
+		fgets(line, SIZE_LINE, fiGender);
+		while (!feof(fiGender) && correct) {									                                                                                                                                
+			if (sscanf_s(line, "%d,%d,%d,%d,%d", &user.code, &user.age, &user.weight, &user.height, &user.gender) == 5) {
+				subjectsGender[iSubject] = user.gender;
+				iSubject++;
+			}else {
+				correct = false;
+				printf("Erreur a la lecture des genres des sujets");  
 			}
+			fgets(line, SIZE_LINE, fiGender);
 		}
 		fclose(fiGender);
 	}
@@ -37,9 +35,9 @@ void gendersArray(int subjectsGender[]) {
 void writeLineInFile(Movement movementToWrite, FILE* fi) {
 	fprintf(fi, "\n%d,%d,%d", movementToWrite.name, movementToWrite.gender, movementToWrite.index + 1);
 	int iVAcc = 0;
-	while (iVAcc < TIME_EVALUATED) {												// inverser les conditions -> corrigé (voir com suivant) 
-		fprintf(fi, ",%f", movementToWrite.vAcc[iVAcc]);							// c'est un peu bizarre : vous avez rajouté des 0 pour arriver jusqu'à 600
-		iVAcc++;																	// et là vous ne les mettez pas dans le fichier -> corrigé 
+	while (iVAcc < TIME_EVALUATED) {									
+		fprintf(fi, ",%f", movementToWrite.vAcc[iVAcc]);							
+		iVAcc++;																	 
 	}
 }
 void writeLine(Movement movementToWrite, int iTest, int iSub, FILE* fiTrain, FILE* fiTest) {
@@ -54,15 +52,15 @@ void generationFile() {
 
 	FILE* fiTest;
 	FILE* fiTrain;
-	fopen_s(&fiTest, TESTSET, "w");				// pourquoi pas mode w? -> corrigé (avant a+) 
-	fopen_s(&fiTrain, TRAINSET, "w");			// tests si problème?	-> corrigé 
+	fopen_s(&fiTest, TESTSET, "w");				 
+	fopen_s(&fiTrain, TRAINSET, "w");			
 
 	if (fiTest == NULL || fiTrain == NULL) {
 		printf("Probleme lors de l'ouverture d'un des 2 fichiers");
 	} else {
 		char paths[NB_DIR][CHAR_MAX_SIZE] = { "dws_1", "dws_2", "dws_11", "jog_9", "jog_16", "sit_5", "sit_13", "std_6",
 										"std_14", "ups_3", "ups_4", "ups_12", "wlk_7", "wlk_8", "wlk_15" };
-		char pathFiSub[PATH_SIZE];						// grandeurs de tableaux en # define (partout) -> corrigé 
+		char pathFiSub[PATH_SIZE];						
 
 		int people[NB_SUBJECTS];
 		gendersArray(people);
@@ -89,7 +87,7 @@ void generationFile() {
 				// crée le chemin pour le fichier à utiliser 
 				sprintf_s(pathFiSub, sizeof(pathFiSub), "A_DeviceMotion_data/%s/sub_%d.csv", paths[iDir], iSub + 1); ;
 
-				fopen_s(&fiCurrent, pathFiSub, "r");  					// pourquoi r+? -> corrigé 
+				fopen_s(&fiCurrent, pathFiSub, "r");  				
 				if (fiCurrent == NULL) {
 					printf("Erreur a l'ouverture du fichier !");
 				}
@@ -99,13 +97,13 @@ void generationFile() {
 					int iMovement = 0;
 					while (!feof(fiCurrent)) {
 						//lecture du genre de chaque ligne
-						fgets(line, SIZE_LINE, fiCurrent);			// les scanf pour des double c'est lf -> corrigé 
+						fgets(line, SIZE_LINE, fiCurrent);			
 						sscanf_s(line, "%d,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf", &movementRead.index, &movementRead.attitudeRoll, &movementRead.attitudePitch, &movementRead.attitudeYaw, &movementRead.gravityX, &movementRead.gravityY, &movementRead.gravityZ, &movementRead.rotationRX, &movementRead.rotationRY, &movementRead.rotationRZ, &movementRead.userAccX, &movementRead.userAccY, &movementRead.userAccZ);
-						while (!feof(fiCurrent) && iMovement < TIME_EVALUATED) {	// drôle de condition pour tester une fin de fichier !! -> corrigé 
+						while (!feof(fiCurrent) && iMovement < TIME_EVALUATED) {	
 							if (sscanf_s(line, "%d,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf", &movementRead.index, &movementRead.attitudeRoll, &movementRead.attitudePitch, &movementRead.attitudeYaw, &movementRead.gravityX, &movementRead.gravityY, &movementRead.gravityZ, &movementRead.rotationRX, &movementRead.rotationRY, &movementRead.rotationRZ, &movementRead.userAccX, &movementRead.userAccY, &movementRead.userAccZ) == 13) {
 								movementToWrite.vAcc[iMovement] = sqrt((pow(movementRead.userAccX, 2) + pow(movementRead.userAccY, 2) + pow(movementRead.userAccZ, 2)));
 								iMovement++;
-							} else {			// mettre un else au cas où -> corrigé 
+							} else {			
 								printf("Erreur, le document ne contient pas toutes les infos demandées");
 								iMovement += TIME_EVALUATED - iMovement; 
 							}
@@ -116,10 +114,8 @@ void generationFile() {
 							movementToWrite.vAcc[iMovement] = 0;
 							iMovement++;
 						}
-						//iMovement = 0;			//inutile tu le refais après -> corrigé 
 					}
 					writeLine(movementToWrite, iTest, iSub, fiTrain, fiTest);
-					// est-ce utile?? -> oui, le chemin change à chaque tour de boucle -> non
 					iSub++;
 					iIndex++;
 				}
